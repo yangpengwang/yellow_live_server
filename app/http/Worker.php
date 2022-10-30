d38439cc56363e4d4a470ff7003a343bf53f7bd6
yangpengwang <2254240747@qq.com>
2022-10-30 15:19:25 +0800

    聊天室添加心跳

--- app/http/Worker.php
+++ app/http/Worker.php
@@ -4,20 +4,40 @@ declare (strict_types = 1);
 namespace app\http;
 
 use think\worker\Server;
+use Workerman\Lib\Timer;
 
 
 class Worker extends Server
 {
     protected $socket = "websocket://0.0.0.0:2345";
-    // protected static $heartbeat_time = 50;
-    // protected $client_id;
+    protected static $heartbeat_time = 55;
 
 
+    //设置定时器 定时监听心跳
+    public function onWorkerStart($worker){
+        Timer::add(10, function()use($worker){
+            $time_now = time();
+            foreach($worker->connections as $connection) {
+                // 有可能该connection还没收到过消息，则lastMessageTime设置为当前时间
+                if (empty($connection->lastMessageTime)) {
+                    $connection->lastMessageTime = $time_now;
+                    continue;
+                }
+                // 上次通讯时间间隔大于心跳间隔，则认为客户端已经下线，关闭连接
+                if ($time_now - $connection->lastMessageTime > self::$heartbeat_time) {
+                    $connection->close();
+                }
+                
+            }
+        });
+    }
 
     //接收数据的时候触发
     public function onMessage($connection,$data){
+         // 给connection临时设置一个lastMessageTime属性，用来记录上次收到消息的时间
+        $connection->lastMessageTime = time();
         $arr = json_decode($data);
-      
+
         //表示绑定
         if($arr->type == 'bind' && $arr->roomId){
             // 没验证的话把第一个包当做uid（这里为了方便演示，没做真正的验证）
@@ -26,13 +46,16 @@ class Worker extends Server
             * 实现针对特定uid推送数据
             */
             $this->worker->uidConnections[$arr->roomId][$connection->uid] = $connection;
+            $users = $this->worker->uidConnections[$arr->roomId];
+            foreach($users as $conn){
+                $conn->send($data);
+            }
         }
 
-        //群聊天
+        //直播聊天
         if($arr->type == 'text' && $arr->mode == 'group'){
             //获取房间内的用户
             $users = $this->worker->uidConnections[$arr->roomId];
-            
             foreach($users as $conn){
                 $conn->send($data);
             }
@@ -40,13 +63,7 @@ class Worker extends Server
 
     }
 
-    //断开连接的时候触发
-    public function onClose($connection){
-        // echo 'onClose';
-    }
+  
 
-    //客户端连接发生错误的时候触发
-    public function onError($connection){
-        // echo 'onError';
-    }
+
 }
